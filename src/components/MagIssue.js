b0b1101023e7c4eca3412c37d14077f0fa63cc94
kfrn <knfrances@gmail.com>
2017-04-05 09:45:11 +1200

    Use nocoverimg.jpg for issues where there are no cover images

--- src/components/MagIssue.js
+++ src/components/MagIssue.js
@@ -2,14 +2,15 @@ import React from 'react'
 const getDateString = require('../utilities/getDateString')
 
 const MagIssue = ({props}) => {
-  const imgURL = `images/magazine-covers/${props.derivJPEG}`
+  let imgURL
+  props.derivJPEG ? imgURL = `images/magazine-covers/${props.derivJPEG}` : imgURL = 'images/nocoverimg.jpg'
   const dateString = getDateString(props.day, props.mo, props.year)
   return (
     <div className='magazine-issue'>
-      <a href={props.startPage}><img src={imgURL} alt={`Picture Play ${dateString}`}/></a>
+      <a href={props.startPage} target='_blank'><img src={imgURL} alt={`Picture Play ${dateString}`}/></a>
       <p>Vol {props.vol}, no. {props.number}</p>
       <p>{dateString}</p>
-      <p><a href={props.startPage}>View issue</a></p>
+      <p><a href={props.startPage} target='_blank'>View issue</a></p>
     </div>
   )
 }
