b25b308e7d05e36b10ff3b821decec9e77766ea0
kfrn <knfrances@gmail.com>
2017-04-16 18:01:54 +1200

    Fix typo; CSS tweak

--- src/components/PicturePlay.js
+++ src/components/PicturePlay.js
@@ -5,8 +5,10 @@ const PicturePlayInfo = () => {
     <div className="info-box">
       <h5>Picture Play</h5>
       <p>
-        <span className='magazine-title'>Picture Play</span>, originally titled <span className='magazine-title'>Picture-Play Weekly</span>, was an American weekly magazine focusing on the film industry, first published on April 10, 1915. By the end of the year its name had changed to <span className='magazine-title'>Picture-Play Magazine</span>, and in March 1916 it switched to a monthly format, taking its final name of <span className='magazine-title'>Picture Play</span>. It ended its publication run when it merged with <span className='magazine-title'>Charm Magazine</span> in 1941.<br />
-        <span className='magazine-title'>Picture Play</span> is considered one the more memorable film magazines of the first half of the 20th century.
+        <span className='magazine-title'>Picture Play</span>, originally titled <span className='magazine-title'>Picture-Play Weekly</span>, was an American weekly magazine focusing on the film industry, first published on April 10, 1915. By the end of the year its name had changed to <span className='magazine-title'>Picture-Play Magazine</span>, and in March 1916 it switched to a monthly format, taking its final name of <span className='magazine-title'>Picture Play</span>. It ended its publication run when it merged with <span className='magazine-title'>Charm Magazine</span> in 1941.
+      </p>
+      <p>
+        <span className='magazine-title'>Picture Play</span> is considered one of the more memorable film magazines of the first half of the 20th century.
       </p>
     </div>
   )
